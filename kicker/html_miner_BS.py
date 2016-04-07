cf33b47f6d5f27db8a0f0ad649a1a4a84e9118aa
jdegene <j-f-d@gmx.de>
2016-04-07 16:27:33 +0200

    minor changes

--- kicker/html_miner_BS.py
+++ kicker/html_miner_BS.py
@@ -7,10 +7,28 @@ from bs4 import BeautifulSoup
 
 Spieltag = 1
 
-myFile = 'D:/Test/kicker/1BL_' + str(Spieltag) +'_241.txt'
+myFile = 'D:/Test/kicker/1BL_' + str(Spieltag) +'_721.txt'
 
 htmlData = open(myFile,'r').read()
 
 soup = BeautifulSoup(htmlData)
 
-entry = soup.find(summary='Ranking')
+# Find the 'Ranking' table and move to child tbody
+entry = soup.find(summary='Ranking').tbody
+
+for tr_elem in entry("tr"):
+    
+    a_elem = tr_elem('a')
+    for elem in a_elem:
+        kickerURL = elem.get('href')
+        kickerID = kickerURL[kickerURL.find('manid')+6 : kickerURL.find('/', kickerURL.find('manid')+6)]
+        kickerName = elem.text
+        #print(kickerURL, kickerID)
+    
+    td_elem = tr_elem('td', class_="alignright last")
+    for elem in td_elem:
+        kickerPoints = elem.text
+        
+        print(kickerID, kickerName,  kickerPoints)
+        
+
