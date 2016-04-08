c05455f7aa70e14b38fb3bb830f9f1c2589afd62
jdegene <j-f-d@gmx.de>
2016-04-08 09:59:35 +0200

    code documented

--- kicker/html_miner_BS.py
+++ kicker/html_miner_BS.py
@@ -4,31 +4,39 @@
 # as the xml.etree solution throws errors due to encoding problems this replaces it
 
 from bs4 import BeautifulSoup
+import sqlite3
 
-Spieltag = 1
+gameDay = 10
 
-myFile = 'D:/Test/kicker/1BL_' + str(Spieltag) +'_721.txt'
+myFile = 'D:/Test/kicker3/1BL_' + str(gameDay) +'_511.txt'
 
 htmlData = open(myFile,'r').read()
 
-soup = BeautifulSoup(htmlData)
+# Define Parent element, use lxml parser
+soup = BeautifulSoup(htmlData, "lxml")
 
 # Find the 'Ranking' table and move to child tbody
 entry = soup.find(summary='Ranking').tbody
 
+# iterate over all <tr> elements
 for tr_elem in entry("tr"):
     
+    # find 'a' elements = links
     a_elem = tr_elem('a')
+    
+    # iterate over the sub-elements and extract tag link and text
     for elem in a_elem:
         kickerURL = elem.get('href')
+        # extract the UserID from the URL
         kickerID = kickerURL[kickerURL.find('manid')+6 : kickerURL.find('/', kickerURL.find('manid')+6)]
         kickerName = elem.text
-        #print(kickerURL, kickerID)
-    
+
+    # find the points gained on every gameDay in the 'td' tag with attribute 'alignright last'
     td_elem = tr_elem('td', class_="alignright last")
     for elem in td_elem:
         kickerPoints = elem.text
         
         print(kickerID, kickerName,  kickerPoints)
         
-
+# connect to SQLite3 DB
+conDB = sqlite3.connect('D:/Test/kicker_db/test1.db')
